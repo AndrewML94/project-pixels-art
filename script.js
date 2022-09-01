acd7a11b5f7c89607695469621907f0b93bfd78b
Andrew Lima <andrewmll94@gmail.com>
2022-09-01 15:37:57 -0300

    Conclusão do 6° e 7° requisito

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const button = document.getElementById('button-random-color');
 const div = document.querySelectorAll('.color');
-const divColor1 = div[0].style.backgroundColor = '#000000';
-const divColor2 = div[1].style.backgroundColor = '#FF0000';
-const divColor3 = div[2].style.backgroundColor = '#008000';
-const divColor4 = div[3].style.backgroundColor = '#0000FF';
+div[0].style.backgroundColor = '#000000';
+div[1].style.backgroundColor = '#FF0000';
+div[2].style.backgroundColor = '#008000';
+div[3].style.backgroundColor = '#0000FF';
 
 function changeButtonColor() {
   const hexadecimal = '0123456789ABCDEF';
@@ -19,7 +19,11 @@ function changeButtonColor() {
     div[1].style.backgroundColor = color;
     div[2].style.backgroundColor = color2;
     div[3].style.backgroundColor = color3;
+    var backgroundColors = [div[0].style.backgroundColor, div[1].style.backgroundColor, div[2].style.backgroundColor, div[3].style.backgroundColor];
   }
+  localStorage.setItem('colorPalette', JSON.stringify(backgroundColors));
+  div[1] = JSON.parse(localStorage.getItem('colorPalete'));
 }
 
 button.addEventListener('click', changeButtonColor) 
+
